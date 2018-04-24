9b309802dd4b4d6e8a44e23ee212abe83c4f8e32
Brandyn Adderley <madderle@gmail.com>
2018-04-24 11:44:29 -0500

    reduced componenets

--- Analysis/src/Analysis.py
+++ Analysis/src/Analysis.py
@@ -194,7 +194,7 @@ print("Starting Dimension Reduction...")
 
 
 # Dimension Reduction
-pipe = Pipeline(steps=[('svd', TruncatedSVD(100000)),
+pipe = Pipeline(steps=[('svd', TruncatedSVD(50000)),
                        ('norm', Normalizer(copy=False))
                        ])
 tweets_transform = pipe.fit_transform(tweets_tfidf)
