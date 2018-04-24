26014319a8664e02f5b87791a72e5bcae5b918f4
Brandyn Adderley <madderle@gmail.com>
2018-04-24 09:47:50 -0500

    Increased the number of components to 150,000 isntead of 15,000

--- Analysis/src/Analysis.py
+++ Analysis/src/Analysis.py
@@ -194,7 +194,7 @@ print("Starting Dimension Reduction...")
 
 
 # Dimension Reduction
-pipe = Pipeline(steps=[('svd', TruncatedSVD(15000)),
+pipe = Pipeline(steps=[('svd', TruncatedSVD(150000)),
                        ('norm', Normalizer(copy=False))
                        ])
 tweets_transform = pipe.fit_transform(tweets_tfidf)
