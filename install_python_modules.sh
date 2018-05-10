86a88463b778f929bcf8ca18c92a3c8155eb92b1
Brandyn Adderley <madderle@gmail.com>
2018-05-10 15:22:34 -0500

    added git install and pip3 install

--- install_python_modules.sh
+++ install_python_modules.sh
@@ -3,12 +3,16 @@
 export PYSPARK_PYTHON=/usr/bin/python3
 export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
 
+# Install git
+sudo yum install git -y
+
+
 # Non-standard and non-Amazon Machine Image Python modules:
 sudo pip install -U \
   boto              \
   tweet-preprocessor
 
 # Install in Python 3
-sudo pip3 install -U \
+sudo pip-3.4 install -U \
   boto              \
   tweet-preprocessor
